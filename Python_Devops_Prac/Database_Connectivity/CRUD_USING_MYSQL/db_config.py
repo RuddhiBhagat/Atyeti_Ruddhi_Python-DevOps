45ea6c4244426d7240e797716dcf5da2f9d5c822
RuddhiBhagat <ruddhi.bhagat@atyeti.com>
2025-06-24 16:31:52 +0530

    Delete Python_Devops_Prac directory

--- Python_Devops_Prac/Database_Connectivity/CRUD_USING_MYSQL/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def getConnection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='1234',
-        database='sampledb'
-    )

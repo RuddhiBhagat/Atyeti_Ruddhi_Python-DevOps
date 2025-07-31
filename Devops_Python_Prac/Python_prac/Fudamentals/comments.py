77d55cd48a41ceebcc21191b5ffb7ea1a1f676b9
RuddhiBhagat <ruddhi.bhagat@atyeti.com>
2025-07-31 12:36:47 +0530

    Add files via upload

---
+++ Devops_Python_Prac/Python_prac/Fudamentals/comments.py
@@ -0,0 +1,7 @@
+# This is a single-line comment
+
+
+"""
+This program asks the user for their name
+and age, then prints a greeting message.
+"""
---
+++ Devops_Python_Prac/Python_prac/Fudamentals/ip_op.py
@@ -0,0 +1,12 @@
+#Using input()
+#Take input from the user.
+name = input("Enter your name: ")
+
+#Using print()
+#Display output to the console.
+print("Welcome to the Python world!")
+
+#Using formatted strings (f-strings)
+#Display values inside strings in a clean way.
+age = int(input("Enter your age: "))
+print(f"You are {age} years old.")
